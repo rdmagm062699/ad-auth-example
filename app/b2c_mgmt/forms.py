f0b035d79927ea57204bbd8b1b8114d68f23d1bf
adammartin <ebondark.od@gmail.com>
2018-04-02 14:00:30 -0400

    Added a custom validator for franchise number

--- app/b2c_mgmt/forms.py
+++ app/b2c_mgmt/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
-from wtforms.validators import Required, NumberRange, Length, Email
+from wtforms.validators import Required, NumberRange, Length, Email, ValidationError
+from flask_login import current_user
 
 class CreateUserForm(FlaskForm):
     franchise_number = IntegerField('Franchise Number', [Required(message='You must enter a valid integer for a franchise number'), NumberRange(0,4000)])
     first_name = StringField('First Name', [Required(message="Please enter a first name"), Length(2)])
     last_name = StringField('Last Name', [Required(message="Please enter a last name"), Length(2)])
     email = StringField('Email', [Required(message="Please enter a valid e-mail address"), Email()])
+
+    def validate_franchise_number(form, field):
+        # As far as I can tell you can only do limited validation types for cases where you need current_user ... weird
+        if not field.data in current_user.franchises():
+            raise ValidationError('You may only create users that you are authorized for {}'.format(current_user.franchises()))
